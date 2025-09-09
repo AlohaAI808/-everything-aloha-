16cd254da185723179f8b657de73f48a8d557e58
AlohaAI808 <alohaaisolutions@gmail.com>
2025-09-09 09:10:31 -1000

    Create islands.js

---
+++ src/data/islands.js
@@ -0,0 +1,26 @@
+export const islands = [
+  {
+    name: "Kauaʻi",
+    image: "/images/kauai.jpg",
+    description: "The Garden Isle - lush landscapes and dramatic cliffs",
+    link: "/islands/kauai"
+  },
+  {
+    name: "Oʻahu",
+    image: "/images/oahu.jpg", 
+    description: "The Heart of Hawaii - vibrant culture and iconic beaches",
+    link: "/islands/oahu"
+  },
+  {
+    name: "Maui",
+    image: "/images/maui.jpg",
+    description: "The Valley Isle - stunning sunrises and luxury resorts",
+    link: "/islands/maui"
+  },
+  {
+    name: "Big Island",
+    image: "/images/big-island.jpg",
+    description: "The Big Island - active volcanoes and diverse landscapes",
+    link: "/islands/big-island"
+  }
+];
